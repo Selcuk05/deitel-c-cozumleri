9626469d04ccdb77a3feaad868142a379078e670
Selcuk05 <selcukxxx12@gmail.com>
2023-11-11 23:13:12 +0300

    C Fonksiyonlar 5.22 - 5.26

--- c_fonksiyonlari.c
+++ c_fonksiyonlari.c
@@ -177,6 +177,131 @@ void _5_20(){
     squarePrintFill(4, '#');
 }
 
+// 5.22
+int bolum(int a, int b){
+    return a / b;
+}
+
+int kalan(int a, int b){
+    return a % b;
+}
+
+void basamak(int sayi){
+    if(sayi == 0){
+        return;
+    }
+
+    int k = kalan(sayi, 10);
+    int b = bolum(sayi, 10);
+
+    basamak(b);
+
+    printf("%d  ", k);
+}
+
+void _5_22(){
+    unsigned short int sayi;
+    printf("Sayi giriniz: ");
+    scanf("%d", &sayi);
+
+    basamak(sayi);
+}
+
+// 5.23
+int gecenSaniye(int saat, int dakika, int saniye){
+    int toplamSaniye = 0;
+    if(saat >= 12){
+        saat -= 12;
+    }
+    toplamSaniye += saat * 60 * 60;
+    toplamSaniye += dakika * 60;
+    toplamSaniye += saniye;
+    return toplamSaniye;
+}
+
+void _5_23(){
+    int saat, dakika, saniye;
+    printf("Saat giriniz: ");
+    scanf("%d", &saat);
+    printf("Dakika giriniz: ");
+    scanf("%d", &dakika);
+    printf("Saniye giriniz: ");
+    scanf("%d", &saniye);
+    printf("Son 12\'den itibaren gecen saniye: %d", gecenSaniye(saat, dakika, saniye));
+}
+
+// 5.24
+double celcius(double fahrenheitDegree){
+    double c = (fahrenheitDegree - 32) * 5 / 9;
+    return c;
+}
+
+double fahrenheit(double celciusDegree){
+    double f = (celciusDegree * 9 / 5) + 32;
+    return f;
+}
+
+void _5_24(){
+    printf("Celcius\tFahrenheit\n");
+    for(int i = 0; i <= 100; i++){
+        printf("%d\t%lf\n", i, fahrenheit(i));
+    }
+
+    printf("\n");
+    printf("Fahrenheit\tCelcius\n");
+    for(int i = 32; i <= 212; i++){
+        printf("%d\t%lf\n", i, celcius(i));
+    }
+}
+
+// 5.25
+double enKucuk(double a, double b, double c){
+    double enKucuk;
+    if(b < a){
+        enKucuk = b;
+        if(c < b){
+            enKucuk = c;
+        }
+    }else{
+        enKucuk = a;
+        if(c < a){
+            enKucuk = c;
+        }
+    }
+
+    return enKucuk;
+}
+
+void _5_25(){
+    printf("Sayilar: %.1f, %.1f, %.1f\n", 4.2, 3.5, 6.8);
+    printf("En kucuk: %.1f", enKucuk(4.2, 3.5, 6.9));
+}
+
+// 5.26
+int perfect(int number){
+    int sum = 0;
+    for(int i = 1; i < number; i++){
+        if(number % i == 0){
+            sum += i;
+        }
+    }
+    return sum == number;
+}
+
+void _5_26(){
+    for(int i = 1; i <= 1000; i++){
+        if(perfect(i)){
+            printf("%d Mukemmel Sayi: ", i);
+            for(int j = 1; j < i; j++){
+                if(i % j == 0){
+                    printf("%d ", j);
+                }
+            }
+            printf("\n");
+        }
+    }
+}
+
 int main(void){
     // _5_9();
     // _5_10();
@@ -186,6 +311,11 @@ int main(void){
     // _5_17();
     // _5_18();
     // _5_19();
-    _5_20();
+    // _5_20();
+    // _5_22();
+    // _5_23();
+    // _5_24();
+    // _5_25();
+    _5_26();
     return 0;
 }
